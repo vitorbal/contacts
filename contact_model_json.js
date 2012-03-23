c6c3b54feedb617aa8f627577840f98949418dcc
Gijora Dammann <gijora.dammann@googlemail.com>
2012-03-23 14:05:28 +0100

    contact model json mockup added

---
+++ contact_model_json.js
@@ -0,0 +1,65 @@
+[
+    {
+        "phonebook_fk":"1",
+        "name":"John Doe",
+        "number":"+123123123",
+        "email":"boss@company.com",
+        "tags":"mobile-dev; CEO",
+        "image":"images/picture.jpg",
+        "notes":"cool guy, just never in the office"
+    },
+    {
+        "phonebook_fk":"1",
+        "name":"Meyyer",
+        "number":"+23423432434",
+        "email":"meyyer@company.com",
+        "tags":"nodejs; backend",
+        "image":"images/picture2.jpg",
+        "notes":"cool guy, just never in the office"
+    },
+    {
+        "phonebook_fk":"1",
+        "name":"Michael",
+        "number":"+09834098509348",
+        "email":"michael@company.com",
+        "tags":"frontend; backebone",
+        "image":"images/picture3.jpg",
+        "notes":"cool guy, just never in the office"
+    },
+    {
+        "phonebook_fk":"1",
+        "name":"Gijiora",
+        "number":"+9872394234",
+        "email":"gijora@company.com",
+        "tags":"mobile-dev; backbone",
+        "image":"images/picture4.jpg",
+        "notes":"cool guy, just never in the office"
+    },
+    {
+        "phonebook_fk":"1",
+        "name":"Gijora",
+        "number":"+9872394234",
+        "email":"gijora@company.com",
+        "tags":"mobile-dev; backbone",
+        "image":"images/picture4.jpg",
+        "notes":"cool guy, just never in the office"
+    },
+    {
+        "phonebook_fk":"1",
+        "name":"Vitor",
+        "number":"+898989898",
+        "email":"vitor@company.com",
+        "tags":"mobile-dev; backbone",
+        "image":"images/picture5.jpg",
+        "notes":"cool guy, just never in the office"
+    },
+    {
+        "phonebook_fk":"1",
+        "name":"Robert",
+        "number":"+982394",
+        "email":"robert@company.com",
+        "tags":"backend; nodejs; c-sharp",
+        "image":"images/picture6.jpg",
+        "notes":"cool guy, just never in the office"
+    }
+]
