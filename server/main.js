8939bb1d849d79cfe129d7e0686c1ae0597c9e0e
Robert G. Siebeck <robert@siebeck.org>
2012-03-23 17:20:54 +0100

    JSONP compatibility

--- server/main.js
+++ server/main.js
@@ -2,28 +2,28 @@ var entities = require("./entities");
 var server = require("./server");
 
 server.router.map(function () {
-	this.get(/^contacts$/).bind(function (req, res) {
+	this.get(/^contacts$/).bind(function (req, res, d) {
 		entities.getContacts(function (err, docs) {
-			res.send(200, {}, docs);
+			res.sendJSONP(d.callback, docs);
 		});
 	});
 	
 	this.post(/^contacts/).bind(function(req, res, data) {
 		var contact = entities.createContact(data.phonebookId, data.first, data.last, data.phone, data.email, data.note);
-		res.send(200, {}, contact);
+		res.sendJSONP(data.callback, contact);
 	});
 
 	this.get(/^phonebooks$/).bind(function(req,res,d) {
-		entities.getPhonebooks(function (err, docs) { res.send(200,{}, docs);})
+		entities.getPhonebooks(function (err, docs) { res.sendJSONP(d.callback, docs);})
 	});
 
 	this.post(/^phonebook$/).bind(function (req,res,d) {
-		res.send(200, {}, entities.createPhonebook(d.name, d.password));
+		res.sendJSONP(d.callback, entities.createPhonebook(d.name, d.password));
 	});
 
 	this.get(/^phonebook\/([0-9a-f]+)$/).bind(function(req,res,id) {
 		entities.getPhonebook(id, function (e,d) {
-			res.send(200, {}, d);
+			res.sendJSONP(d.callback, d);
 		});
 	});
 });
