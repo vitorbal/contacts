dddb968e6f4fecf334d5a78d7c284bc013baa30e
meyyu <meyyu@meyyu.(none)>
2012-03-23 15:27:43 +0100

    post - create contact,post testing html

--- server/main.js
+++ server/main.js
@@ -9,6 +9,11 @@ router.map(function () {
 			res.send(200, {}, docs);
 		});
 	});
+	
+	this.post(/^contacts/).bind(function(req, res, data) {
+		var contact = entities.createContact(data.first, data.last, data.phone, data.email, data.note);
+		res.send(200, contact)
+	});
 });
 
 require('http').createServer(function (request, response) {
