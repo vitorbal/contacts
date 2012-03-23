1d2edc36c986df02c020201fb91092b7b1acbd4a
robert <robert@rgs-lenovo.(none)>
2012-03-23 15:34:48 +0100

    Split into server.js

--- server/main.js
+++ server/main.js
@@ -1,9 +1,7 @@
 var entities = require("./entities");
+var server = require("./server");
 
-var journey = require('journey');
-var router = new (journey.Router);
-
-router.map(function () {
+server.router.map(function () {
 	this.get(/^contacts$/).bind(function (req, res) {
 		entities.ContactModel.find({}, function (err, docs) {
 			res.send(200, {}, docs);
@@ -12,18 +10,6 @@ router.map(function () {
 	
 	this.post(/^contacts/).bind(function(req, res, data) {
 		var contact = entities.createContact(data.first, data.last, data.phone, data.email, data.note);
-		res.send(200, contact)
+		res.send(200, {}, contact);
 	});
 });
-
-require('http').createServer(function (request, response) {
-	var body = "";
-
-	request.addListener('data', function(chunk) { body += chunk });
-	request.addListener('end', function() {
-		router.handle(request, body, function (result) {
-			response.writeHead(result.status, result.headers);
-			response.end(result.body);
-		});
-	});
-}).listen(8080);
---
+++ server/server.js
@@ -0,0 +1,17 @@
+var journey = require('journey');
+var router = new (journey.Router);
+
+
+require('http').createServer(function (request, response) {
+	var body = "";
+
+	request.addListener('data', function(chunk) { body += chunk });
+	request.addListener('end', function() {
+		router.handle(request, body, function (result) {
+			response.writeHead(result.status, result.headers);
+			response.end(result.body);
+		});
+	});
+}).listen(8080);
+
+module.exports = {router : router};
