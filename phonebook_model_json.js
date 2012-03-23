79037a4cd10918f8ffe663a2a39e52004de38084
Gijora Dammann <gijora.dammann@googlemail.com>
2012-03-23 14:10:35 +0100

    phonebook model json mockup added

---
+++ phonebook_model_json.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name":"hackanoon phonebook",
+        "id":1
+        "password":"hacking",
+        "image":"images/picture8.jpg"
+    },
+    {
+        "name":"hackanoon phonebook",
+        "id":2
+        "password":"passwort",
+        "image":"images/picture9.jpg"
+    }
+]
